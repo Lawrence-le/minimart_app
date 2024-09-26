33eda4ce7c8d2f9ba7b3793068045b2a933b8974
lawrence <lawrence_mklee@hotmail.com>
2024-09-26 22:53:43 +0800

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: parseInt(process.env.PORT, 10) || 5173,
+    host: true, // Bind to all network interfaces
+  },
+});
